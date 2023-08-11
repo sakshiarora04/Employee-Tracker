5d337df1ac69003f4b2efda7f030be5016791422
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-11 20:00:22 +1000

    viewl all queries add

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,16 @@
+SELECT * from department;
+
 SELECT role.id,role.title, department.department_name AS department,role.salary
 FROM role
 LEFT JOIN department
 ON role.department_id = department.id
 ORDER BY role.id;
 
--- SELECT * from role;
+SELECT e.id, e.first_name, e.last_name, r.title, d.department_name AS department, r.salary, CONCAT(m.first_name," ",m.last_name) As manager
+FROM employee e
+LEFT JOIN role r
+ON e.role_id= r.id
+LEFT JOIN department d
+ON r.department_id= d.id
+LEFT JOIN employee m
+ON e.manager_id= m.id;
