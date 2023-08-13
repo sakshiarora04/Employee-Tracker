bb4fb04e7fc7394c508c31f1005161bd66d4556a
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-14 01:33:23 +1000

    update employees

--- server.js
+++ server.js
@@ -26,16 +26,16 @@ class Query {
             this.viewEmployeesByManager();
             break;
           case "View employees by department":
-            addRole();
+            this.viewEmployeesByDepartment();
             break;
           case "View the total utilized budget of a department":
-            addRole();
+            this.viewEmployeesBudget();
             break;
           case "Update Employee Role":
-            addRole();
+            this.updateEmployeeRole();
             break;
           case "Update Employee Manager":
-            addRole();
+            this.updateEmployeeManagers();
             break;
           case "Add Department":
             this.addDepartment();
@@ -72,13 +72,13 @@ class Query {
   }
   // View all departments
    viewDepartments() {
-    const sql = `SELECT id, department_name AS title FROM department`;      
+    const sql = `SELECT id, department_name AS title FROM department ORDER BY id`;      
       db.query(sql, (err, rows) => {
         if (err) {
           console.log({ error: err.message });
           return;
         }
-        console.table("All Departments", rows);  
+        console.table("\n  All Departments", rows);  
         this.main();       
       });      
   }
@@ -94,7 +94,7 @@ class Query {
       console.log({ error: err.message });
       return;
     }
-    console.table("All roles", rows);    
+    console.table("\n  All roles", rows);    
     this.main(); 
   }); 
   
@@ -140,7 +140,7 @@ viewEmployeesByManager(){
                     console.log(err.message);
                     return;
                   }
-                  console.table(`All employees of manager '${params[0]}'`, rows);    
+                  console.table(`\n All employees of manager '${params[0]}'`, rows);    
                   this.main();                   
                 });                 
       });     
@@ -148,7 +148,64 @@ viewEmployeesByManager(){
 }
 // View employees by department
 viewEmployeesByDepartment(){
+  let departments=[];
+  const sqlDepartment='SELECT department_name FROM department';
   
+  db.promise().query(sqlDepartment)
+  .then(([department])=>{      
+    departments=department.map((d)=>{
+      return d['department_name'];        
+    })       
+      cli.viewDepartment(departments)
+      .then((body)=>{          
+        const sql = `SELECT e.id,CONCAT(e.first_name," ",e.last_name) AS Employees, r.title FROM employee e
+        JOIN role r
+        ON e.role_id=r.id
+        JOIN department d   
+        ON r.department_id=d.id
+        WHERE d.department_name=?
+        ORDER BY e.first_name`;
+        const params = [body.departmentName];
+        db.query(sql, params, (err, rows) => {
+                  if (err) {
+                    console.log(err.message);
+                    return;
+                  }
+                  console.table(`\n All employees of department '${params[0]}'`, rows);    
+                  this.main();                   
+                });                 
+      });     
+  });
+}
+//View the total utilized budget of a department
+viewEmployeesBudget(){
+  let departments=[];
+  const sqlDepartment='SELECT department_name FROM department';
+  
+  db.promise().query(sqlDepartment)
+  .then(([department])=>{      
+    departments=department.map((d)=>{
+      return d['department_name'];        
+    })       
+      cli.viewDepartment(departments)
+      .then((body)=>{          
+        const sql = `SELECT d.department_name AS Department, SUM(r.salary) AS Total_Salaries FROM employee e
+        JOIN role r
+        ON e.role_id=r.id
+        JOIN department d   
+        ON r.department_id=d.id
+        WHERE d.department_name=?`;
+        const params = [body.departmentName];
+        db.query(sql, params, (err, rows) => {
+                  if (err) {
+                    console.log(err.message);
+                    return;
+                  }
+                  console.table(`\n Total utilized budget of department '${params[0]}'`, rows);    
+                  this.main();                   
+                });                 
+      });     
+  });
 }
 //Add department
 addDepartment(){
@@ -162,7 +219,7 @@ addDepartment(){
       console.log({ error: err.message });
       return;
     }    
-    console.log(`Added ${params[0]} to database`);
+    console.log(`\n Added ${params[0]} to database`);
     this.viewDepartments(); 
   });
     })  
@@ -182,7 +239,7 @@ getDepartmentArray(){
            .catch((err)=>{
             console.log(err);
             reject();
-           })
+           });
 
           });
      }
@@ -203,7 +260,7 @@ getDepartmentArray(){
               console.log(err.message);
               return;
             }
-            console.log(`Added ${params[0]} to database`);
+            console.log(`\n Added ${params[0]} to database`);
             this.viewRoles(); 
           });
         });
@@ -234,18 +291,115 @@ getDepartmentArray(){
         cli.addEmployeeQuestions(employeeArr)
         .then((body)=>{          
            const sql = `INSERT INTO employee (first_name,last_name,role_id,manager_id)
-            VALUES( ?,?,(SELECT role.id from role  WHERE role.title=?),(SELECT e.id from employee e WHERE CONCAT(e.first_name," ",e.last_name)=?))`;
-          const params = [body.first, body.last, body.role, body.manager];
+            VALUES( ?,?,(SELECT role.id from role  WHERE role.title=?),?)`;
+          const params = [body.first, body.last, body.role, body.manager+1];
           db.query(sql, params, (err, result) => {
                     if (err) {
                       console.log(err.message);
                       return;
                     }
-                    console.log(`Added ${params[0]} ${params[1]} to database`);
+                    console.log(`\n Added ${params[0]} ${params[1]} to database`);
                     this.viewEmployees();
                   });                 
         });     
     });  
+}
+updateEmployeeRole() {
+  const empRoleArr=[];   
+  let employeeList=[];  
+  let roleList=[]; 
+  const sqlRole='SELECT title from role ORDER BY id';
+    const sqlEmployee='SELECT CONCAT(first_name," ",last_name) As emp FROM employee';
+    db.promise().query(sqlRole)
+    .then(([roles])=>{      
+      roleList=roles.map((role)=>{
+        return role['title'];        
+      })  
+      empRoleArr.push(roleList)    
+    })
+    db.promise().query(sqlEmployee)
+    .then(([employees])=>{      
+      employeeList=employees.map((employee)=>{
+        return employee['emp'];        
+      })
+      empRoleArr.push(employeeList);
+        cli.updateEmployee(empRoleArr)
+        .then((body)=>{          
+           const sql = `UPDATE employee
+           SET role_id=?
+           WHERE id=?`;
+          const params = [body.newRole+1, body.employeeName+1];
+          db.query(sql, params, (err, result) => {
+                    if (err) {
+                      console.log(err.message);
+                      return;
+                    }
+                    console.log(`\n Updated ${params[0]} for ${params[1]} to database`);
+                    this.viewEmployees();
+                  });                 
+        });     
+    });  
+    
+}
+// Update employee managers
+updateEmployeeManagers() {
+  const empArr=[];   
+  let employeeList=[];  
+  let managerList=[]; 
+  let empList=[];
+  let manList=[];
+  const sqlManager='SELECT CONCAT(first_name," ",last_name) AS manager from employee';
+    const sqlEmployee='SELECT CONCAT(first_name," ",last_name) As emp FROM employee ORDER BY id';
+    db.promise().query(sqlManager)
+    .then(([managers])=>{      
+      managerList=managers.map((manager)=>{
+        return manager['manager'];        
+      }) 
+       managerList.forEach(m,i => {
+        const obj={};       
+        obj.name=m;
+        obj.value=i+1;
+         manList.push(obj);
+       });
+      empArr.push(manList)    
+    })
+    db.promise().query(sqlEmployee)
+    .then(([employees])=>{      
+      employeeList=employees.map((employee)=>{
+        return employee['emp'];        
+      })
+      
+      for (let i = 0; i < employeeList.length; i++) {
+       let obj={};       
+       obj.name=employeeList[i];
+       obj.value=i+1;
+        empList.push(obj);
+      }
+      empArr.push(empList);  
+      console.log(empArr)    
+        cli.updateEmpManager(empArr)
+        .then((body)=>{          
+           const sql = `Update employee 
+           SET manager_id=?
+           WHERE id=?`;
+          const params = [body.newManager, body.employeeName];  
+          console.log(params)               
+          if(params[0]===params[1]){
+            console.log('Invalid manager selected');
+            this.main();
+            return;
+          }
+          db.query(sql, params, (err, result) => {
+                    if (err) {
+                      console.log(err.message);
+                      return;
+                    }
+                   console.log(`\n Updated manager`);
+                    this.viewEmployees();
+                  });                 
+        });     
+    });  
+    
 }
 }
 const query = new Query();
